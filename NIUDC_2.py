caea53c265c92e54fe27b91093fa532d341f86e8
kamil20020 <kamdyw@wp.pl>
2021-03-26 23:51:44 +0100

    Zaimplementowano do końca metodę stop and wait i informacje o błędach podczas transmisji

--- NIUDC_2.py
+++ NIUDC_2.py
@@ -1,6 +1,19 @@
 import random
 import math
 
+def makeParityBit(packet):
+
+    numberOfOnes = 0
+
+    for i in packet:
+        if(i == 1):
+            numberOfOnes += 1
+
+    if(numberOfOnes % 2 == 0):
+        return 0
+    else:
+        return 1
+
 class Generator:
 
     @staticmethod
@@ -38,69 +51,114 @@ class Coder:
         return preparedSignal
 
     @staticmethod
-    def makeParityBit(packet):
+    def codePacketWithParity(packet):
 
-        numberOfOnes = 0
+        packet.append(makeParityBit(packet))
 
-        for i in packet:
-            if(i == 1):
-                numberOfOnes += 1
-
-        if(numberOfOnes % 2 == 0):
-            return 0
-        else:
-            return 1
-
-    @staticmethod
-    def codeSignal(signal):
-
-        for i in range(len(signal)):
-            signal[i].append(Coder.makeParityBit(signal[i]))
-
-        return signal
+        return packet
 
 class Noise:
 
     @staticmethod
-    def noiseSignal(signal, changeChance):
+    def noisePacket(packet, changeChance):
 
-        preparedSignal = []
+        for i in range(len(packet)):
 
-        for i in signal:
+            rand = random.randint(0,100)
 
-            packet = []
+            if(rand > 100 - changeChance):
 
-            for j in i:
+                if(packet[i] == 1):
+                    packet[i] = 0
+                else:
+                    packet[i] = 1
 
-                rand = random.randint(0,100)
+        return packet
 
-                if(rand >= 100 - changeChance):
-
-                    if(j == 1):
-                        j = 0
-                    else:
-                        j = 1
-
-                packet.append(j)
-
-            preparedSignal.append(packet)
-
-        return preparedSignal
 
 class Application:
 
+    def stopAndWait(self, signal):
+
+        parityBitMatch = False
+
+        howMuchRepeats = []
+
+        notDetectedErrors = 0
+
+        for i in range(len(signal)):
+            howMuchRepeats.append(0)
+
+        x = int(input("Wprowadz 0 dla kodowania z dodaniem bitu parzystosci lub 1 dla kodowania przy pomocy algorytmu CRC: "))
+
+        for i in range(len(signal)): 
+
+            if(x == 0):
+
+                signal[i] = Coder.codePacketWithParity(self.signal[i]) 
+                print("\nZakodowany (bit parzystosci) pakiet numer ", i, ": ", signal[i], "\n")
+
+                previousPacket = []
+
+                previousPacket = signal.copy()
+
+            while parityBitMatch == False:
+
+                signal[i] = Noise.noisePacket(self.signal[i], 10)
+
+                if(howMuchRepeats[i] == 0):
+
+                    print("Przesłany pakiet numer ", i, ": ", signal[i], "\n")
+
+                else:
+                    print("Powtórnie przesłany pakiet numer ", i, ": ", signal[i], "\n")
+
+                if(makeParityBit(signal[i]) == 1):
+
+                    howMuchRepeats[i] += 1
+
+                else:
+
+                    if(previousPacket != signal[i]):
+                        notDetectedErrors += 1
+
+                    parityBitMatch = True
+
+            parityBitMatch = False
+
+            signal[i].pop(len(signal[i])-1)
+
+            print("-----------\n")
+
+        packetsWithMoreThanFourRepeats = 0
+
+        for i in howMuchRepeats:
+
+            if(i > 4):
+                packetsWithMoreThanFourRepeats += 1
+
+        print("Przesłano ", howMuchRepeats.count(0), " pakietów bezbłędnie")
+        print("Przesłano ", howMuchRepeats.count(1), " pakietów, które wymagały jednego powtórzenia")
+        print("Przesłano ", howMuchRepeats.count(2), " pakietów, które wymagały dwóch powtórzeń")
+        print("Przesłano ", howMuchRepeats.count(3), " pakietów, które wymagały trzech powtórzeń")
+        print("Przesłano ", howMuchRepeats.count(4), " pakietów, które wymagały czterech powtórzeń")
+        print("Przesłano ", packetsWithMoreThanFourRepeats, " pakietów, które wymagały więcej niż cztery powtórzenia")
+        print("Wystąpiło ", notDetectedErrors, " błędów niewykrytych")
+
+        return signal
+
     def run(self):
-        self.signal = Generator.generateSignal(20)
-        print(self.signal)
 
-        self.signal = Coder.divideSignal(self.signal, 3)
-        print(self.signal)
+        self.signal = Generator.generateSignal(100)
+        print("Sygnał: \n", self.signal, "\n")
 
-        self.signal = Coder.codeSignal(self.signal)
-        print(self.signal)
+        self.signal = Coder.divideSignal(self.signal, 8)
+        print("Podzielony sygnał: \n", self.signal, "\n")
 
-        self.signal = Noise.noiseSignal(self.signal, 10)
-        print(self.signal)
+        x = int(input("Wprowadź 0 aby wykonać symulację stopAndWhite: "))
+
+        if(x == 0):
+            self.stopAndWait(self.signal)
 
 
 app = Application()
